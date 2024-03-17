b4b41efe202571e78e4e96360b1ac6ef215ffb9a
TakanariShimbo <takanari.shimbo@gmail.com>
2024-03-17 22:29:24 +0900

    add create blog page

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,21 +1,23 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-		<header className="py-5 px-10 border-b flex justify-between items-center">
-			<div>
-				<h1 className="text-2xl font-extrabold">
-					<Link href="/">My Blog</Link>
-				</h1>
-			</div>
-			<div>
-				<nav className="text-sm font-medium">
-					<Link href="/artivles/new" className="bg-orange-300 px-3 py-3 rounded-md">記事を書く</Link>
-				</nav>
-			</div>
-		</header>
+    <header className="py-5 px-10 border-b flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl font-extrabold">
+          <Link href="/">My Blog</Link>
+        </h1>
+      </div>
+      <div>
+        <nav className="text-sm font-medium">
+          <Link href="/articles/new" className="bg-orange-300 px-3 py-3 rounded-md">
+            記事を書く
+          </Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 
-export default Header
+export default Header;
