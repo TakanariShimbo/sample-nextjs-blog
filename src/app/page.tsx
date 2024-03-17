ea14e36a2952f44d8197ba74c7185b20bab83e70
TakanariShimbo <takanari.shimbo@gmail.com>
2024-03-17 20:05:15 +0900

    update pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import ArticleList from "./components/ArticleList";
+import SideMenu from "./components/SideMenu";
+
 export default function Home() {
-  return <div>編集中</div>;
-}
+  return (
+    <div className="md:flex">
+      <section className="w-full md:w-2/3 flex flex-col items-center px-3">
+        <ArticleList/>
+      </section>
+
+      <SideMenu/>
+    </div>
+  );
+};
