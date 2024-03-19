31b697fd96e10af1a2ff9c03a53d5426c79e0a5f
TakanariShimbo <takanari.shimbo@gmail.com>
2024-03-19 14:33:59 +0900

    add article page

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -13,7 +13,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
       {articles.map((article) => (
         <article className="shadow my-4" key={article.id}>
           <Link href={`articles/${article.id}`} className="hover:opacity-75">
-            <Image src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="" width={1280} height={300} />
+            <Image src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`} alt="" width={1280} height={300} />
           </Link>
           <div className="bg-white flex flex-col justify-start p-6">
             <Link href="#" className="text-blue-700 pb-4 font-bold">
--- src/blogAPI.tsx
+++ src/blogAPI.tsx
@@ -1,5 +1,6 @@
 import { cache } from "react";
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" });
@@ -12,4 +13,20 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, { next: { revalidate: 60 } });
+
+  if (res.status === 404) {
+    notFound();
+  }
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  const article = await res.json();
+  return article;
 };
