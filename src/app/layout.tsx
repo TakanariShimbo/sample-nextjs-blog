1d900b384dba918caa9d090543d2e1bc293f59f4
TakanariShimbo <takanari.shimbo@gmail.com>
2024-03-19 15:24:53 +0900

    modify layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`container mx-auto bg-slate-700 text-slate-50 ${inter.className}`}>
-        <Header />
-        {children}
-        <Footer />
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
